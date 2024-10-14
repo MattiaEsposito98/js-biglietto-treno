874e3d647d4ced4543e189d3acdc5dd1604cac24
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-14 17:08:58 +0200

    cambiato 21/100 on 0.21 e assegnata tipo alla variabile

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ console.log(`I km da percorrere sono: ${km_viaggio}`)
 const eta = parseInt(prompt ("Inserite la vostra età: "))
 console.log(`L'età del passegero è: ${eta}`)
 
-const prezzo_al_km = 21 /100;
+const prezzo_al_km = 0.21
 
 if (eta < 18) {
-  prezzo_scontato20 = (km_viaggio * prezzo_al_km) *20/100
+  const prezzo_scontato20 = (km_viaggio * prezzo_al_km) *20/100
   console.log (`Il prezzo totale con lo sconto del 20% è: ${prezzo_scontato20}€`)
 
 } else if (eta >= 65) {
-  prezzo_scontato40 = (km_viaggio * prezzo_al_km) *40 /100
+  const prezzo_scontato40 = (km_viaggio * prezzo_al_km) *40 /100
   console.log (`Il prezzo totale con lo sconto del 40% è: ${prezzo_scontato40}€`)
 }
 
