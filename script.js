d49e658546580bb4354000ba9d5fb9718fffacfe
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-15 10:06:43 +0200

    fix prezzo finale con tofixed

--- script.js
+++ script.js
@@ -10,16 +10,18 @@ const prezzo_al_km = 0.21
 
 if (eta < 18) {
   const prezzo_scontato20 = (km_viaggio * prezzo_al_km) *20/100
-  console.log (`Il prezzo totale con lo sconto del 20% è: ${prezzo_scontato20}€`)
+  let prezzo_finale = (km_viaggio * prezzo_al_km) - prezzo_scontato20
+  console.log (`Il prezzo totale con lo sconto del 20% è: ${prezzo_finale.toFixed(2)}€`)
 
 } else if (eta >= 65) {
   const prezzo_scontato40 = (km_viaggio * prezzo_al_km) *40 /100
-  console.log (`Il prezzo totale con lo sconto del 40% è: ${prezzo_scontato40}€`)
+  let prezzo_finale = (km_viaggio * prezzo_al_km) - prezzo_scontato40
+  console.log (`Il prezzo totale con lo sconto del 40% è: ${prezzo_finale.toFixed(2)}€`)
 }
 
 else {
   let prezzo_pieno = km_viaggio * prezzo_al_km
-  console.log (`Il prezzo totale è: ${prezzo_pieno}€`)
+  console.log (`Il prezzo totale è: ${prezzo_pieno.toFixed(2)}€`)
 }
 
 
